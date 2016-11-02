adfc84ad40914549b630d4c68f0f42d9d6ce926e
jdlawrence <jamil.lawrence@gmail.com>
2016-11-01 19:02:51 -0700

    Added functions to check plays against computer plays and let the player know when they lose

--- container/App.js
+++ container/App.js
@@ -7,6 +7,15 @@ import { Router,
 import Left                 from            './Left';
 import Right                from           './Right';
 
+function comparePlays(arr1, arr2) {
+  var len = arr1.length > arr2.length ? arr2.length : arr1.length; 
+  for (var i = 0; i < len; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true;
+}
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -15,9 +24,21 @@ export default class App extends React.Component {
         plays: [],
         computerPlays: ['blue', 'red', 'green', 'yellow', 'yellow', 'green', 'red', 'blue'],
         playersTurn: false,
-        gameClock: null
+        gameClock: null,
+        youLose: false
     };
     this.generatePlays = this.generatePlays.bind(this);
+    this.comparePlays = this.comparePlays.bind(this);
+  }
+
+  comparePlays(arr1, arr2) {
+    var len = arr1.length > arr2.length ? arr2.length : arr1.length; 
+    for (var i = 0; i < len; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false
+      }
+    }
+    return true;
   }
   timesTwo() {
     this.setState({counter: this.state.counter * 2});
@@ -50,7 +71,16 @@ export default class App extends React.Component {
   }
   pushPlays(color) {
     var temp = this.state.plays.concat(color);
-    this.setState({plays: temp}, () => { console.log('plays: ', this.state.plays); });
+    this.setState({plays: temp}, () => { 
+      console.log('plays: ', this.state.plays); 
+      console.log('computerPlays: ', this.state.computerPlays); 
+
+      // If the player plays don't match the computer plays, trigger youLose to true
+      if (!this.comparePlays(this.state.computerPlays, this.state.plays)) {
+        this.setState({youLose: true});
+      }
+      console.log('comparePlays: ', this.comparePlays(this.state.computerPlays, this.state.plays));
+    });
   }
   render () {
     return (
@@ -58,6 +88,7 @@ export default class App extends React.Component {
       <div> 
       Simon Game!
       <div>{this.state.counter}</div>
+      { this.state.youLose ? <div>YOU LOSE</div> : null }
       <button onClick={this.timesTwo.bind(this)}>timesTwo</button>
       <button onClick={this.generatePlays.bind(this)}>Start</button>
       <button onClick={this.stopClock.bind(this)}>Stop</button>
@@ -70,7 +101,7 @@ export default class App extends React.Component {
               logColor: this.logColor.bind(this),
               pushPlays: this.pushPlays.bind(this),
               counter: this.state.counter,
-              computerPlays: this.state.computerPlays
+              computerPlays: this.state.computerPlays,
             })}
       </div>
     );
