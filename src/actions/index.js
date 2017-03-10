151103c2db888ca299229dc4c563a8a935b2f33c
jdlawrence <jamil.lawrence@gmail.com>
2017-03-10 11:56:59 -0800

    Refactoring pushing computer plays

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,4 @@
 export const toggleColor = (color) => {
-  console.log('color', color);
   return {
     type: 'TOGGLE_COLOR',
     color
@@ -11,18 +10,23 @@ export const playSequence = () => {
   // it must return function
   return function (dispatch, getState) {
     var compPlays = getState().gameState.dummyPlays;
+    // var compPlays = getState();
+    // console.log('state', compPlays.gameState);
     for (let i = 0; i < compPlays.length; i++) {
       setTimeout(() => {
         // Asynchronous action must be dispatched
         dispatch(toggleColor(compPlays[i]));
-        setTimeout( () => {
+        setTimeout(() => {
           dispatch(toggleColor(compPlays[i]));
         }, 300);
       }, 600 * i);
     }
   };
 };
-
+export const pushCompPlay = (randColor) => ({
+  type: 'PUSH_COMP_PLAY',
+  color: randColor
+});
 export const pushPlay = (color) => ({
   type: 'PUSH_PLAY',
   color: color
@@ -40,19 +44,27 @@ export const startGame = (count) => {
   return function (dispatch, getState) {
     var plays = ['green', 'red', 'blue', 'yellow'];
     var randColor = plays[Math.floor(Math.random() * 4)];
-    dispatch({
-      type: 'START_GAME',
-      color: randColor
-    });
+    
+    // Reset all settings;
+    dispatch(clearState());
+
+    // Push a play for the computer
+    dispatch(pushCompPlay(randColor));
+
+    // Playback the sequence of computer plays
+    dispatch(playSequence());
+
+    // Wait some time for the human to play
+
     setTimeout(function () {
       dispatch(comparePlays());
-      dispatch(endRound());
-      if (getState().youLose) {
-        console.log('it\s over!!!: ');
-      }
-      else {
-        dispatch(startGame(count + 1));
-      }
+      // dispatch(endRound());
+      // if (getState().youLose) {
+      //   console.log('it\s over!!!: ');
+      // }
+      // else {
+      //   dispatch(startGame(count + 1));
+      // }
     }, count * 1000 + 5000);
 
   };
