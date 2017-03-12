3e9239273b5fc93a84c14fc436bcb2afcfa5c4a9
jdlawrence <jamil.lawrence@gmail.com>
2017-03-12 15:19:06 -0700

    Seperated out gameState reducer

---
+++ src/reducers/gameState.js
@@ -0,0 +1,61 @@
+import {
+  PUSH_HUMAN_PLAY,
+  COMPARE_PLAYS,
+  PUSH_COMP_PLAY,
+  END_ROUND,
+  CLEAR_STATE
+} from '../actions';
+
+import comparePlays from '../utils/comparePlays';
+
+const gameState = (state = {
+  plays: [],
+  computerPlays: [],
+  dummyPlays: ['blue', 'red', 'green', 'yellow'],
+  youLose: false,
+  roundCount: 0
+}, action) => {
+  if (action.type === PUSH_HUMAN_PLAY) {
+    return {
+      ...state, 
+      plays: state.plays.concat(action.color)
+    };
+  }
+  else if (action.type === COMPARE_PLAYS) {
+    if (comparePlays(state.computerPlays, state.plays)) {
+      return state;
+    }
+    else {
+      return {
+        ...state, youLose: true
+      };
+    }
+  }
+  else if (action.type === PUSH_COMP_PLAY) {
+    return {
+      ...state, 
+      computerPlays: state.computerPlays.concat(action.color)
+    };
+  }
+  else if (action.type === END_ROUND) {
+    return {
+      ...state,
+      plays: [],
+      roundCount: state.roundCount + 1
+    };
+  }
+  else if (action.type === CLEAR_STATE) {
+    return {
+      ...state, 
+      plays: [],
+      computerPlays: [],
+      youLose: false,
+      roundCount: 0
+    };  
+  } else {
+    return state;
+  }
+
+};
+
+export default gameState;
