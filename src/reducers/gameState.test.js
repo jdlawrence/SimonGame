48662d1311798b04d9305c6c5c8fe9293d39ddd5
jdlawrence <jamil.lawrence@gmail.com>
2017-04-05 16:53:59 -0700

    Tests in progress

--- src/reducers/gameState.test.js
+++ src/reducers/gameState.test.js
@@ -1,7 +1,16 @@
 import comparePlays from './comparePlays';
+import { PUSH_HUMAN_PLAY } from '../actions';
+import reducer, { initialState } from './gameState';
 
 describe('Methods of the gameState reducer', () => {
   it('FIX THIS TEST', () => {
     expect(8).toEqual(2);
   });
+
+  it(`handles action ${PUSH_HUMAN_PLAY}`, () => {
+    expect(reducer(initialState, pushHumanPlay('blue')))
+      .toEqual({ ...initialState, blue: true });
+  });
+
+
 })
