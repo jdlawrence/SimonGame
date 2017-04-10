2eea5d8fc16ed3811a595d45fa4f0adbaccc9ca7
jdlawrence <jamil.lawrence@gmail.com>
2017-04-09 21:49:49 -0700

    Added compare plays test

--- src/utils/comparePlays.test.js
+++ src/utils/comparePlays.test.js
@@ -1,7 +1,14 @@
 import comparePlays from './comparePlays';
 
 describe('Compare plays helper function', () => {
-  it('FIX THIS TEST', () => {
-    expect(1).toEqual(2);
+  it('Expects comparePlays to return false if either human or comp array is empty', () => {
+    var compPlays = [];
+    var humanPlays = [];
+    expect(comparePlays(compPlays, humanPlays)).toEqual(false);
+  });
+  it('Expects comparePlays to accurately compare populated arrays', () => {
+    var compPlays = ['red', 'blue', 'green'];
+    var humanPlays = ['red', 'blue', 'green'];
+    expect(comparePlays(compPlays, humanPlays)).toEqual(true);
   });
 })
