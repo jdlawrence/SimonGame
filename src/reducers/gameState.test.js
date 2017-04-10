e4b4f348935b4c767474aff717159bb8d93d69fc
jdlawrence <jamil.lawrence@gmail.com>
2017-04-09 21:49:31 -0700

    Added tests for push play

--- src/reducers/gameState.test.js
+++ src/reducers/gameState.test.js
@@ -1,16 +1,22 @@
-import comparePlays from './comparePlays';
-import { PUSH_HUMAN_PLAY } from '../actions';
+import { PUSH_HUMAN_PLAY, pushHumanPlay } from '../actions';
 import reducer, { initialState } from './gameState';
 
 describe('Methods of the gameState reducer', () => {
-  it('FIX THIS TEST', () => {
-    expect(8).toEqual(2);
+  it(`handles action ${PUSH_HUMAN_PLAY} with a single action`, () => {
+    expect(reducer(initialState, pushHumanPlay('blue')).plays)
+      .toEqual(['blue']);
+    expect(reducer(initialState, pushHumanPlay('red')).plays)
+      .toEqual(['red'])
+    expect(reducer(initialState, pushHumanPlay('green')).plays)
+      .toEqual(['green'])
+    expect(reducer(initialState, pushHumanPlay('yellow')).plays)
+      .toEqual(['yellow'])
   });
 
-  it(`handles action ${PUSH_HUMAN_PLAY}`, () => {
-    expect(reducer(initialState, pushHumanPlay('blue')))
-      .toEqual({ ...initialState, blue: true });
-  });
-
+  it(`handles multiple calls to ${PUSH_HUMAN_PLAY}`, () => {
+    var newState = reducer(initialState, pushHumanPlay('red'));
+    expect(reducer(newState, pushHumanPlay('green')).plays)
+    .toEqual(['red', 'green']);
+  })
 
 })
