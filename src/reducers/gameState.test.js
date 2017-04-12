63a7b72b623f75d4d884ddfb00276293cc096f6a
jdlawrence <jamil.lawrence@gmail.com>
2017-04-12 07:21:07 -0700

    rename variable and create test computer plays

--- src/reducers/gameState.test.js
+++ src/reducers/gameState.test.js
@@ -1,22 +1,30 @@
-import { PUSH_HUMAN_PLAY, pushHumanPlay } from '../actions';
+import {
+  PUSH_HUMAN_PLAY, pushHumanPlay,
+  PUSH_COMP_PLAY, pushCompPlay,
+} from '../actions';
 import reducer, { initialState } from './gameState';
 
 describe('Methods of the gameState reducer', () => {
-  it(`handles action ${PUSH_HUMAN_PLAY} with a single action`, () => {
-    expect(reducer(initialState, pushHumanPlay('blue')).plays)
+  it(`handles action ${PUSH_HUMAN_PLAY} with a single invocation`, () => {
+    expect(reducer(initialState, pushHumanPlay('blue')).humanPlays)
       .toEqual(['blue']);
-    expect(reducer(initialState, pushHumanPlay('red')).plays)
+    expect(reducer(initialState, pushHumanPlay('red')).humanPlays)
       .toEqual(['red'])
-    expect(reducer(initialState, pushHumanPlay('green')).plays)
+    expect(reducer(initialState, pushHumanPlay('green')).humanPlays)
       .toEqual(['green'])
-    expect(reducer(initialState, pushHumanPlay('yellow')).plays)
+    expect(reducer(initialState, pushHumanPlay('yellow')).humanPlays)
       .toEqual(['yellow'])
   });
 
   it(`handles multiple calls to ${PUSH_HUMAN_PLAY}`, () => {
     var newState = reducer(initialState, pushHumanPlay('red'));
-    expect(reducer(newState, pushHumanPlay('green')).plays)
-    .toEqual(['red', 'green']);
+    expect(reducer(newState, pushHumanPlay('green')).humanPlays)
+      .toEqual(['red', 'green']);
+  })
+
+  it(`handles calls to ${PUSH_COMP_PLAY}`, () => {
+    expect(reducer(initialState, pushCompPlay('blue')).compPlays)
+      .toEqual(['blue'])
   })
 
 })
