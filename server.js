c11e7ab91f78de2d84dac50db3c9bed0d2820244
jdlawrence <jamil.lawrence@gmail.com>
2017-06-11 08:29:06 -0700

    Added PORT as an environment variable

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+const PORT = process.env.PORT || 5040;
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(5040, 'localhost', function (err, result) {
+}).listen(PORT, 'localhost', function (err, result) {
   if (err) {
     return console.log('888888', err);
   }
