9ee8b886704fc9615b41ea055ef32bb293198a9f
jdlawrence <jamil.lawrence@gmail.com>
2017-06-14 15:53:04 -0700

    Adds production config

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+const express = require('express');
+const path = require('path');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
 const PORT = process.env.PORT || 5040;
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const app = express();
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(PORT, 'localhost', function (err, result) {
-  if (err) {
-    return console.log('888888', err);
-  }
+if (isDeveloping) {
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+  }).listen(PORT, 'localhost', function (err, result) {
+    if (err) {
+      return console.log('888888', err);
+    }
 
-  console.log(`Listening at http://localhost:${PORT}/`);
-});
+    console.log(`Listening at http://localhost:${PORT}/`);
+  });
+} else {
+  app.use(express.static(__dirname + '/dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+  app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}/`);
+  })
+}
