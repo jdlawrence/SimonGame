ea39f5c0a402307f23a17e720a271944f8575478
jdlawrence <jamil.lawrence@gmail.com>
2017-06-14 16:22:34 -0700

    Adds production webpack config

---
+++ webpack.production.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+const PORT = process.env.PORT || 5040;
+
+module.exports = {
+  devtool: 'source',
+
+  entry: [
+    './src/index.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': "'production'"
+      }
+    }),
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
+};
