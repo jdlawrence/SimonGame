61d38ace60840cfea6ddf715a7cc10f13da502db
jdlawrence <jamil.lawrence@gmail.com>
2017-07-22 14:39:24 -0700

    Highscores load from API

--- src/components/Highscores.js
+++ src/components/Highscores.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import ReactDOM from         'react-dom';
+import ReactDOM from 'react-dom';
 
 export default class Highscores extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+  }
+  componentDidMount() {
+    this.props.fetchScores();
   }
   render() {
- 
-    return (
-      <div >
-        <h1>
+    if (this.props.dataService.scores.length === 0) {
+      return (
+        <div >
+          <h1>
             Highscores component
         </h1>
-      </div>
-    );
+        </div>
+      );
+    } else {
+      const row = this.props.dataService.scores.map((data, index) =>
+        <tr key={index}>
+          <td key={data.player}>{data.player}</td>
+          <td key={data.Time}>{data.Time}</td>
+          <td key={data.score}>{data.score}</td>
+        </tr>
+      );
+      return (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Player</th>
+                <th>Time</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+            {row}
+            </tbody>
+          </table>
+        </div>
+      )
+    }
   }
 }
 
