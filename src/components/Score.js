87a273fc3793e595190eb8e9daa44301787915f7
jdlawrence <jamil.lawrence@gmail.com>
2017-07-22 14:39:48 -0700

    Cleans up score display

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,23 +1,17 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
-import request from 'request';
 
 class Score extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
-  // // componentDidMount() {
-  // //   // request('/api', (error, response, body) => {
-  // //   //   console.log('error:', error); // Print the error if one occurred
-  // //   //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // //   //   console.log('body:', body); // Print the HTML for the Google homepage. 
-  // //   // });
-  // // }
+  constructor(props) {
+    super(props);
+  }
+  
   render() {
-
     return (
-      <div className="score show-border">Score: {this.props.score}</div>
+      <div className="score show-border">
+        <div>Score: {this.props.score}</div>
+        <div>Game over? {'' + this.props.youLose}</div>
+      </div>
     );
   }
 }
